07163e4e49b92a47db0f44c16dc525cacbab178e
Vadym Shevchenko <82364676+Vadim93-code@users.noreply.github.com>
2023-04-15 21:48:16 +0100

    Delete src directory

--- src/main/java/com/example/weather/adapter/CityItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weather.adapter
-
-data class CityItem(
-    val cityName: String,
-    val lat: String,
-    val lon: String
-)
--- src/main/java/com/example/weather/adapter/Item.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weather.adapter
-
-data class Item(
-    val day: String,
-    val status: String,
-    val temp_max: String,
-    val temp_min: String
-)
